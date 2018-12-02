4af9e4382b502be5618c9e8f233d7686250a9f46
anthonygeronimo01297@gmail.com <anthonygeronimo01297@gmail.com>
2018-12-02 20:15:57 +0800

    Accomplished the presentation layout

---
+++ mvgisa/mvgisa/PresentationForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace mvgisa
+{
+    partial class PresentationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kryptonPanel1 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel2 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonDropButton1 = new ComponentFactory.Krypton.Toolkit.KryptonDropButton();
+            this.kryptonLabel3 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kryptonDropButton2 = new ComponentFactory.Krypton.Toolkit.KryptonDropButton();
+            this.kryptonDropButton3 = new ComponentFactory.Krypton.Toolkit.KryptonDropButton();
+            this.kryptonContextMenuItems1 = new ComponentFactory.Krypton.Toolkit.KryptonContextMenuItems();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.kryptonDropButton3);
+            this.kryptonPanel1.Controls.Add(this.kryptonDropButton2);
+            this.kryptonPanel1.Controls.Add(this.kryptonButton1);
+            this.kryptonPanel1.Controls.Add(this.kryptonLabel3);
+            this.kryptonPanel1.Controls.Add(this.kryptonDropButton1);
+            this.kryptonPanel1.Controls.Add(this.kryptonLabel2);
+            this.kryptonPanel1.Controls.Add(this.kryptonLabel1);
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.PanelBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.HeaderPrimary;
+            this.kryptonPanel1.Size = new System.Drawing.Size(1348, 299);
+            this.kryptonPanel1.TabIndex = 0;
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.kryptonLabel1.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.TitleControl;
+            this.kryptonLabel1.Location = new System.Drawing.Point(323, 60);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(195, 35);
+            this.kryptonLabel1.TabIndex = 0;
+            this.kryptonLabel1.Values.Text = "Key Result Areas";
+            // 
+            // kryptonLabel2
+            // 
+            this.kryptonLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.kryptonLabel2.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.TitleControl;
+            this.kryptonLabel2.Location = new System.Drawing.Point(242, 110);
+            this.kryptonLabel2.Name = "kryptonLabel2";
+            this.kryptonLabel2.Size = new System.Drawing.Size(284, 35);
+            this.kryptonLabel2.TabIndex = 2;
+            this.kryptonLabel2.Values.Text = "Core Indicator/ Indicator";
+            // 
+            // kryptonDropButton1
+            // 
+            this.kryptonDropButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.kryptonDropButton1.Location = new System.Drawing.Point(588, 51);
+            this.kryptonDropButton1.Name = "kryptonDropButton1";
+            this.kryptonDropButton1.Size = new System.Drawing.Size(487, 44);
+            this.kryptonDropButton1.TabIndex = 3;
+            this.kryptonDropButton1.Values.Text = "kryptonDropButton1";
+            // 
+            // kryptonLabel3
+            // 
+            this.kryptonLabel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.kryptonLabel3.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.TitleControl;
+            this.kryptonLabel3.Location = new System.Drawing.Point(414, 162);
+            this.kryptonLabel3.Name = "kryptonLabel3";
+            this.kryptonLabel3.Size = new System.Drawing.Size(104, 35);
+            this.kryptonLabel3.TabIndex = 5;
+            this.kryptonLabel3.Values.Text = "Critirion";
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonButton1.Location = new System.Drawing.Point(0, 244);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(1348, 55);
+            this.kryptonButton1.StateNormal.Back.Color1 = System.Drawing.Color.ForestGreen;
+            this.kryptonButton1.StateNormal.Back.Color2 = System.Drawing.Color.ForestGreen;
+            this.kryptonButton1.StateNormal.Border.Color1 = System.Drawing.Color.Black;
+            this.kryptonButton1.StateNormal.Border.Color2 = System.Drawing.Color.White;
+            this.kryptonButton1.StateNormal.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonButton1.StateNormal.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.kryptonButton1.StateNormal.Content.ShortText.Color2 = System.Drawing.Color.White;
+            this.kryptonButton1.StateNormal.Content.ShortText.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonButton1.TabIndex = 7;
+            this.kryptonButton1.Values.Text = "Search";
+            this.kryptonButton1.Click += new System.EventHandler(this.kryptonButton1_Click);
+            // 
+            // kryptonDropButton2
+            // 
+            this.kryptonDropButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.kryptonDropButton2.Location = new System.Drawing.Point(588, 101);
+            this.kryptonDropButton2.Name = "kryptonDropButton2";
+            this.kryptonDropButton2.Size = new System.Drawing.Size(487, 44);
+            this.kryptonDropButton2.TabIndex = 8;
+            this.kryptonDropButton2.Values.Text = "kryptonDropButton2";
+            // 
+            // kryptonDropButton3
+            // 
+            this.kryptonDropButton3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.kryptonDropButton3.Location = new System.Drawing.Point(588, 151);
+            this.kryptonDropButton3.Name = "kryptonDropButton3";
+            this.kryptonDropButton3.Size = new System.Drawing.Size(487, 44);
+            this.kryptonDropButton3.TabIndex = 9;
+            this.kryptonDropButton3.Values.Text = "kryptonDropButton3";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(81, 340);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1183, 666);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // PresentationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 1037);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.kryptonPanel1);
+            this.Name = "PresentationForm";
+            this.Text = "PresentationForm";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            this.kryptonPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel3;
+        private ComponentFactory.Krypton.Toolkit.KryptonDropButton kryptonDropButton1;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonDropButton kryptonDropButton3;
+        private ComponentFactory.Krypton.Toolkit.KryptonDropButton kryptonDropButton2;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton1;
+        private ComponentFactory.Krypton.Toolkit.KryptonContextMenuItems kryptonContextMenuItems1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+
+    }
+}
---
+++ mvgisa/mvgisa/PresentationForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace mvgisa
+{
+    public partial class PresentationForm : Form
+    {
+        public PresentationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void kryptonButton1_Click(object sender, EventArgs e)
+        {
+            KryptonDropButton db = new KryptonDropButton();
+            db.Dock = DockStyle.Left;
+            db.Width = this.flowLayoutPanel1.Width;
+            db.Height = 50;
+            db.ButtonStyle = ButtonStyle.ListItem;
+            db.ForeColor = Color.Black;
+            db.StateNormal.Border.Color1 = Color.Cyan;
+            db.StateNormal.Border.DrawBorders = PaletteDrawBorders.All;
+            db.StateNormal.Border.ColorAlign = PaletteRectangleAlign.Inherit;
+
+            KryptonButton db1 = new KryptonButton();
+            db1.Dock = DockStyle.Right;
+            db1.Width = this.flowLayoutPanel1.Width - 50;
+            db1.Height = 50;
+            db1.ButtonStyle = ButtonStyle.ListItem;
+
+            this.flowLayoutPanel1.Controls.Add(db);
+            this.flowLayoutPanel1.Controls.Add(db1);
+            this.flowLayoutPanel1.Controls.SetChildIndex(db1, 1);
+        }
+
+        private void kryptonDropButton4_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
